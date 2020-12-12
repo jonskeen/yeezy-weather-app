3debdae33b4fcc5154d8378e0e73fbcbd8a5cdfc
Jon Skeen <jon.skeen@cayuse.com>
2020-12-11 19:00:15 -0800

    Set up main layout

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
+import CommonPadding from "components/CommonPadding";
+import PaddedCell from "components/PaddedCell";
 
 import styles from "./styles.css";
 
-const Dashboard = props => {
+const Dashboard = () => {
+	const pressureCell = useMemo(() => <PaddedCell>Pressure</PaddedCell>, []);
+	const chanceOfRainCell = useMemo(() => <PaddedCell>Chance of Rain</PaddedCell>, []);
+	const temperatureCell = useMemo(() => <PaddedCell>Temperature</PaddedCell>, []);
+	const amountOfRainCell = useMemo(() => <PaddedCell>Amount of Rain</PaddedCell>, []);
+
+	const renderDesktopLayout = () => {
+		return (
+			<div className={`${styles.layout} ${styles.desktop}`}>
+				<div className={styles.row}>
+					{pressureCell}
+					{chanceOfRainCell}
+				</div>
+
+				<div className={styles.row}>
+					{temperatureCell}
+					{amountOfRainCell}
+				</div>
+			</div>
+		);
+	};
+
+	const renderMobileLayout = () => {
+		return (
+			<div className={`${styles.layout} ${styles.mobile}`}>
+				{pressureCell}
+				{temperatureCell}
+				{chanceOfRainCell}
+				{amountOfRainCell}
+			</div>
+		)
+	};
 
     return (
 		<div data-component="Dashboard" className={styles.dashboard}>
-			<ol>
-				<li>hi</li>
-			</ol>
-
-			<ul>
-				<li>there</li>
-			</ul>
+			<CommonPadding>
+				{renderMobileLayout()}
+				{renderDesktopLayout()}
+			</CommonPadding>
 		</div>
     );
 };
